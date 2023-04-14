d74e3335cd39ff0606735997eaf8934dab35f095
Erika25f <106428479+Erika25f@users.noreply.github.com>
2023-04-14 14:44:52 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+  <!-- Swiper -->
+<div class="swiper mySwiper">
+  <div class="swiper-wrapper">
+    <div class="swiper-slide">
+     <div class="container-fluid mt-3">
+    <div class="row">
+      <div class="col p-3 bg-primary text-white">.imm</div>
+      <div class="col p-3 bg-dark text-white">.imm</div>
+      <div class="col p-3 bg-primary text-white">.imm</div>
+    </div>
+    </div>  
+    </div>
+    <div class="swiper-slide"><img src="https://via.placeholder.com/640x360"></img></div>
+  </div>
+</div>
