4c3c2d3717a55fba58ec513c222bc8f9650e788b
Erika25f <106428479+Erika25f@users.noreply.github.com>
2023-12-21 11:59:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,3 @@
-/* nav prodotti*/
-// JS per la barra di navigazione
-const navbarItems = document.querySelectorAll('.navbar-item');
-const navbarBoxes = document.querySelectorAll('.navbar-box');
-const navbarBoxesArray = Array.from(navbarBoxes);
-
-navbarItems.forEach((navbarItem) => {
-    navbarItem.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        // Aggiunge la classe "active" all'elemento cliccato
-        navbarItems.forEach((item) => {
-            item.classList.remove('active');
-        });
-        navbarItem.classList.add('active');
-
-        // Mostra il box corrispondente all'elemento cliccato e nasconde gli altri
-        const target = navbarItem.getAttribute('data-target');
-        navbarBoxesArray.forEach((box) => {
-            if (box.getAttribute('id') === target) {
-                box.classList.add('active');
-            } else {
-                box.classList.remove('active');
-            }
-        });
-
-        // Mostra il container dei box
-        const navbarBoxesContainer = document.querySelector('.navbar-boxes');
-        navbarBoxesContainer.style.display = 'block';
-    });
-});
-
-
-function addToCart() {
-  var productName = src="https://i.pinimg.com/564x/01/2c/6b/012c6b62e29c42c66a2120aa41cdd133.jpg";
-window.location.href = 'carrello.html';
-}
 
 
 //carrello
@@ -81,23 +44,8 @@ function removeProduct(event) {
 
 
    
-   
-if (productName) {
-        
- 
-var listItem = document.createElement("li");
-        listItem.
-        
-appendChild(document.createTextNode(productName));
-
-        
-
-       
-var cartItems = document.getElementById("cart-items");
-        cartItems.
-        cartIt
-
-      
-appendChild(listItem);
-    }
+function addToCart() {
+  var productName = src="https://i.pinimg.com/564x/01/2c/6b/012c6b62e29c42c66a2120aa41cdd133.jpg";
+window.location.href = 'carrello.html';
+}
 });
