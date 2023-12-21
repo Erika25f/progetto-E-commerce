ae917be299e19bc54483d2230569e5142d11d7d6
Erika25f <106428479+Erika25f@users.noreply.github.com>
2023-12-21 11:48:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,46 @@ function addToCart() {
   var productName = src="https://i.pinimg.com/564x/01/2c/6b/012c6b62e29c42c66a2120aa41cdd133.jpg";
 window.location.href = 'carrello.html';
 }
+
+
+//carrello
+function calculateTotal() {
+  var priceElement = document.querySelector(".card-text.price");
+  var quantityElement = document.querySelector(".form-control.quantity");
+  var subtotalElement = document.getElementById("subtotal");
+  var shippingCostElement = document.getElementById("shippingCost");
+  var totalAmountElement = document.getElementById("totalAmount");
+
+  if (priceElement && quantityElement && subtotalElement && shippingCostElement && totalAmountElement) {
+    var priceString = priceElement.textContent;
+    var price = parseFloat(priceString.replace(/[^0-9.,]/g, "").replace(",", "."));
+    var quantity = parseInt(quantityElement.value);
+    var total = price * quantity;
+
+    var totalElement = quantityElement.parentNode.nextElementSibling;
+    totalElement.textContent = "Totale: €" + total.toFixed(2);
+
+    subtotalElement.textContent = "€" + total.toFixed(2);
+
+    var shippingCost = 2.0; // Inserisci il valore effettivo dei costi di spedizione
+    shippingCostElement.textContent = "€" + shippingCost.toFixed(2);
+
+    var totalAmount = total + shippingCost;
+    totalAmountElement.textContent = "€" + totalAmount.toFixed(2);
+  }
+};
+
+function removeProduct(event) {
+  var card = event.target.closest(".card");
+  card.remove();
+
+  var subtotalElement = document.getElementById("subtotal");
+  var shippingCostElement = document.getElementById("shippingCost");
+  var totalAmountElement = document.getElementById("totalAmount");
+
+  if (subtotalElement && shippingCostElement && totalAmountElement) {
+    subtotalElement.innerHTML = "€0,00";
+    shippingCostElement.innerHTML = "€0,00";
+    totalAmountElement.innerHTML = "€0,00";
+  }
+};
