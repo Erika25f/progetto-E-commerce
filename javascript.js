23481a2bd906921bc9a93f1519ee3f84862b62d4
Erika25f <106428479+Erika25f@users.noreply.github.com>
2024-02-03 10:31:39 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,41 +1,30 @@
-//carrello
 function calculateTotal() {
-  var priceElement = document.querySelector(".card-text.price");
-  var quantityElement = document.querySelector(".form-control.quantity");
-  var subtotalElement = document.getElementById("subtotal");
-  var shippingCostElement = document.getElementById("shippingCost");
-  var totalAmountElement = document.getElementById("totalAmount");
-
-  if (priceElement && quantityElement && subtotalElement && shippingCostElement && totalAmountElement) {
-    var priceString = priceElement.textContent;
-    var price = parseFloat(priceString.replace(/[^0-9.,]/g, "").replace(",", "."));
-    var quantity = parseInt(quantityElement.value);
-    var total = price * quantity;
-
-    var totalElement = quantityElement.parentNode.nextElementSibling;
-    totalElement.textContent = "Totale: €" + total.toFixed(2);
-
-    subtotalElement.textContent = "€" + total.toFixed(2);
-
-    var shippingCost = 2.0; // Inserisci il valore effettivo dei costi di spedizione
-    shippingCostElement.textContent = "€" + shippingCost.toFixed(2);
-
-    var totalAmount = total + shippingCost;
-    totalAmountElement.textContent = "€" + totalAmount.toFixed(2);
-  }
-};
-
-function removeProduct(event) {
-  var card = event.target.closest(".card");
-  card.remove();
-
+  var priceElements = document.querySelectorAll(".card-text.price");
+  var quantityElements = document.querySelectorAll(".form-control.quantity");
   var subtotalElement = document.getElementById("subtotal");
   var shippingCostElement = document.getElementById("shippingCost");
   var totalAmountElement = document.getElementById("totalAmount");
 
   if (subtotalElement && shippingCostElement && totalAmountElement) {
-    subtotalElement.innerHTML = "€0,00";
-    shippingCostElement.innerHTML = "€0,00";
-    totalAmountElement.innerHTML = "€0,00";
+    var subtotal = 0;
+
+    priceElements.forEach(function(priceElement, index) {
+      var priceString = priceElement.textContent;
+      var price = parseFloat(priceString.replace(/[^0-9.,]/g, "").replace(",", "."));
+      var quantity = parseInt(quantityElements[index].value);
+      var total = price * quantity;
+      subtotal += total;
+
+      var totalElement = quantityElements[index].parentNode.nextElementSibling;
+      totalElement.textContent = "Totale: €" + total.toFixed(2);
+    });
+
+    subtotalElement.textContent = "€" + subtotal.toFixed(2);
+
+    var shippingCost = 2.0; // Inserisci il valore effettivo dei costi di spedizione
+    shippingCostElement.textContent = "€" + shippingCost.toFixed(2);
+
+    var totalAmount = subtotal + shippingCost;
+    totalAmountElement.textContent = "€" + totalAmount.toFixed(2);
   }
-};
+}
